bbe247ba53c639d4cd2265a86b335ddfc9627d3b
Mathias007 <mathias.najlepszy@gmail.com>
2021-02-19 20:29:53 +0100

    handle displaying duration and current time of song.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const artist = document.getElementById("artist");
 
 const progressContainer = document.getElementById("progress-container");
 const progress = document.getElementById("progress");
+const currentTimeEl = document.getElementById("current-time");
+const durationEl = document.getElementById("duration");
 
 const music = document.querySelector("audio");
 const prevBtn = document.getElementById("prev");
@@ -97,6 +99,23 @@ function updateProgressBar(e) {
         // Update progress bar width
         const progressPercent = (currentTime / duration) * 100;
         progress.style.width = `${progressPercent}%`;
+        // Calculate display for duration
+        const durationMinutes = Math.floor(duration / 60);
+        let durationSeconds = Math.floor(duration % 60);
+        if (durationSeconds < 10) {
+            durationSeconds = `0${durationSeconds}`;
+        }
+        // Delay switching duration Element to avoid NaN
+        if (durationSeconds) {
+            durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
+        }
+        // Calculate display for current time
+        const currentMinutes = Math.floor(currentTime / 60);
+        let currentSeconds = Math.floor(currentTime % 60);
+        if (currentSeconds < 10) {
+            currentSeconds = `0${currentSeconds}`;
+        }
+        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
     }
 }
 
